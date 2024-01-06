fa6dfe3c172381cdf7f6baed21cffa100d0a9c99
Christof Petig <christof.petig@arcor.de>
2024-01-06 15:33:46 +0100

    next try

--- Makefile
+++ Makefile
@@ -1,6 +1,9 @@
-CFLAGS=-g -fPIC 
+#WS_SOURCE=$(HOME)/Downloads/wireshark-4.0.8
+DEPS=$(shell pkg-config --cflags wireshark)
+CFLAGS=-g -fPIC  $(DEPS) -I.
+#-I$(WS_SOURCE)/ -I$(WS_SOURCE)/obj-x86_64-linux-gnu/ -I$(WS_SOURCE)/include/ $(DEPS)
 LDFLAGS=-shared
 LIBS=
 
-libveloren_ds.so: plugin.c
+libveloren_ds.so: plugin.c veloren.c
 	$(CC) $(CFLAGS) $(LDFLAGS) $^ -o $@ $(LIBS)
---
+++ plugin.c
@@ -0,0 +1,33 @@
+/*
+ * Do not modify this file. Changes will be overwritten.
+ *
+ * Generated automatically from make-plugin-reg.py.
+ */
+
+#include "config.h"
+
+#include <gmodule.h>
+
+/* plugins are DLLs on Windows */
+#define WS_BUILD_DLL
+#include "ws_symbol_export.h"
+
+#include "epan/proto.h"
+
+void proto_register_foo(void);
+void proto_reg_handoff_foo(void);
+
+WS_DLL_PUBLIC_DEF const gchar plugin_version[] = PLUGIN_VERSION;
+WS_DLL_PUBLIC_DEF const int plugin_want_major = WIRESHARK_VERSION_MAJOR;
+WS_DLL_PUBLIC_DEF const int plugin_want_minor = WIRESHARK_VERSION_MINOR;
+
+WS_DLL_PUBLIC void plugin_register(void);
+
+void plugin_register(void)
+{
+    static proto_plugin plug_foo;
+
+    plug_foo.register_protoinfo = proto_register_foo;
+    plug_foo.register_handoff = proto_reg_handoff_foo;
+    proto_register_plugin(&plug_foo);
+}
