5b566a07cdf97eb2e118acb06ef99f4016dcdca7
Christof Petig <christof.petig@arcor.de>
2024-01-06 17:08:05 +0100

    rename to veloren

--- Makefile
+++ Makefile
@@ -2,8 +2,12 @@
 DEPS=$(shell pkg-config --cflags wireshark)
 CFLAGS=-g -fPIC  $(DEPS) -I.
 #-I$(WS_SOURCE)/ -I$(WS_SOURCE)/obj-x86_64-linux-gnu/ -I$(WS_SOURCE)/include/ $(DEPS)
-LDFLAGS=-shared
-LIBS=
+LDFLAGS=-shared 
+#$(shell pkg-config --ldflags wireshark)
+LIBS=$(shell pkg-config --libs wireshark)
 
-libveloren_ds.so: plugin.c veloren.c
+veloren_ds.so: plugin.c veloren.c
 	$(CC) $(CFLAGS) $(LDFLAGS) $^ -o $@ $(LIBS)
+
+install: veloren_ds.so
+	cp $^ $(HOME)/.local/lib/wireshark/plugins/4.0/codecs
--- veloren.c
+++ veloren.c
@@ -1,16 +1,18 @@
 #include "config.h"
 #include <epan/packet.h>
 
-#define FOO_PORT 1234
+#define FOO_PORT 14004
 
 static int proto_foo;
 
 static int
 dissect_foo(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree _U_, void *data _U_)
 {
-    col_set_str(pinfo->cinfo, COL_PROTOCOL, "FOO");
+    col_set_str(pinfo->cinfo, COL_PROTOCOL, "VLR");
     /* Clear the info column */
     col_clear(pinfo->cinfo,COL_INFO);
+
+    proto_item *ti = proto_tree_add_item(tree, proto_foo, tvb, 0, -1, ENC_LITTLE_ENDIAN);
 
     return tvb_captured_length(tvb);
 }
@@ -19,17 +21,17 @@ void
 proto_register_foo(void)
 {
     proto_foo = proto_register_protocol (
-        "FOO Protocol", /* name        */
-        "FOO",          /* short name  */
-        "foo"           /* filter_name */
+        "Veloren Protocol", /* name        */
+        "VLR",          /* short name  */
+        "vlr"           /* filter_name */
         );
 }
 
 void
 proto_reg_handoff_foo(void)
 {
-    static dissector_handle_t foo_handle;
+    dissector_handle_t foo_handle;
 
     foo_handle = create_dissector_handle(dissect_foo, proto_foo);
-    dissector_add_uint("udp.port", FOO_PORT, foo_handle);
+    dissector_add_uint_with_preference("tcp.port", FOO_PORT, foo_handle);
 }
